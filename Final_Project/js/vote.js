c69db07851ef72a0a45456aff0e694afc3ebdb09
PLEgithub <32885466+PLEgithub@users.noreply.github.com>
2018-01-02 18:16:31 -0500

    updated JS script tags

--- Final_Project/js/vote.js
+++ Final_Project/js/vote.js
@@ -1,8 +1,8 @@
-// JS for Responsive Nav bar
-$( '#nav li:has(ul)' ).doubleTapToGo();
-
-
 $('#votesubmit').click(function() {
   alert('Vote Submitted!');
 });
 // Above alert does not seem to be firing...
+
+
+// JS for Responsive Nav bar
+$( '#nav li:has(ul)' ).doubleTapToGo();
